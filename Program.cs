7f7ec059458a5bd208aa74367788f8b3a66061ef
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-02-02 18:22:18 +0300

    Finish

--- Program.cs
+++ Program.cs
@@ -3,3 +3,16 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+System.Console.Write ("Введите число для анализа: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int i=0; // число вариантов
+int b=0; // итоговые числа
+
+System.Console.Write("Получаются следующие числа: ");
+while (i < a / 2 )
+{
+    System.Console.Write($"{b+2}  ");
+    i++;
+    b=b+2;
+}
+System.Console.WriteLine("");
